2c8e9b6fab1ab9fb81b58432b1391ef8c0948608
JadavShubham51 <jadavshubham5@gmail.com>
2023-11-02 15:47:17 +0530

    Javascript-Module-data

---
+++ Javascript-Module/02ext.js
@@ -0,0 +1,6 @@
+document.write("this external JS");
+
+window.alert("Hello Alert"); //error ya alert
+window.confirm("are you sure?"); //yes or no
+window.prompt("5+5 = ?"); //answer and click open
+window.print(); // print file banava
